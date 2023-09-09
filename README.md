ec727832a38e2a40108e8321d920fbaba88dc98a
douglasmatosdev <douglasmatosdev@gmail.com>
2023-09-09 10:36:49 -0300

    create user cceak

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # CCEAK
 Casa de Cultura Espirita Allan Kardec
+
+Required
+- Java 17 ou superior
+- MySQL 8 ou superior
--- backend/src/main/resources/db/migration/V8__Insert_Data_In_Users.sql
+++ backend/src/main/resources/db/migration/V8__Insert_Data_In_Users.sql
@@ -1,3 +1,4 @@
 ﻿INSERT INTO `users` (`user_name`, `full_name`, `password`, `account_non_expired`, `account_non_locked`, `credentials_non_expired`, `enabled`) VALUES
+	('cceak', 'CCEAK', 'cc570288cee80d61d31539a2756027e491f439ea57e6a481a18cd5db17c065baa8c59034da67f093', b'1', b'1', b'1', b'1'),
 	('leandro', 'Leandro Costa', '18772e06d2fba8ae7ec74be05f7d30466c3e749d6a40c4c91eb0e0480c95bd301442e3790a1616e1', b'1', b'1', b'1', b'1'),
 	('flavio', 'Flavio Costa', '826f4acae20c2b206ce1b737bb771251be93f179a251c0bb2df09e0f02db3493d2dbd3e522f607c2', b'1', b'1', b'1', b'1');
